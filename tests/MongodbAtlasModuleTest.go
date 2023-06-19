5ba33320949ba9654467a8dcd9b3780a14ae266b
Suraj Kumar Adhikari <surajprogrammer@live.com>
2023-06-20 00:40:51 +0200

    Task 15 - Terratest integration tests

---
+++ tests/MongodbAtlasModuleTest.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../",
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables and check they have the expected values.
+	databaseVersion := terraform.Output(t, terraformOptions, "mongo_database_version")
+	paused := terraform.Output(t, terraformOptions, "paused")
+
+	assert.Equal(t, "6.0", databaseVersion)
+	assert.Equal(t, "6.0", paused)
+}
